2f243d1f95c11511f2c1367f69d1919ca5b8a031
Varun Verma <varun.ver@gmail.com>
2023-03-23 21:57:23 +1100

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,5 +1,7 @@
 import timeit
 
+import matplotlib.pyplot as plt
+
 from . import Expense
 
 
@@ -19,12 +21,32 @@ def main():
     print(timeit.timeit(stmt="expenses.categorize_for_loop()",
                         setup=
                         '''
-                        from . import Expense
-                        expenses = Expense.Expenses()
-                        expenses.read_expenses('data/spending_data.csv')
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
                         ''',
                         number=100000,
                         globals=globals()))
+
+    print(timeit.timeit(stmt="expenses.categorize_set_comprehension()",
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
+                        number=100000,
+                        globals=globals()))
+
+    fig, ax = plt.subplots()
+    labels = ['Necessary', 'Food', 'Unnecessary']
+    divided_expenses_sum = []
+
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
+
+    ax.pie(divided_expenses_sum, labels=labels, autopct='%1.1f%%')
+    plt.show()
 
 
 if __name__ == "__main__":
